38a24ce876df105c7d262f35b400d5d3f4f99f2b
U+039b <git@0x39b.fr>
2018-02-11 20:55:03 +0100

    - Use dexdump provided by the system - Improve icon retrieval - Add application universal ID - Add list of APK signatures - Add a bit of logging - Add icon perceptual hash
    Signed-off-by: U+039b <git@0x39b.fr>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ install_requires = [
 ]
 
 setup(name = 'exodus_core',
-      version = '1.0.5',
+      version = '1.0.7',
       description = 'Core functionality of εxodus',
       author = 'Exodus Privacy',
       author_email = 'contact@exodus-privacy.eu.org',
