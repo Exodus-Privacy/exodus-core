770c86ce16316df1443b5488a52f6363fcd707a0
U+039b <git@0x39b.fr>
2018-03-16 12:03:00 +0100

    Release 1.0.8

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ install_requires = [
 ]
 
 setup(name = 'exodus_core',
-      version = '1.0.7',
+      version = '1.0.8',
       description = 'Core functionality of εxodus',
       author = 'Exodus Privacy',
       author_email = 'contact@exodus-privacy.eu.org',
