5991745389723db3ed13c41feb045078e0dab2fd
simpnu <simon@pruneau.info>
2019-06-17 21:52:08 +0200

    Remove unused function get_Name

--- exodus_core/analysis/static_analysis.py
+++ exodus_core/analysis/static_analysis.py
@@ -89,33 +89,6 @@ class Certificate:
         self.subject = cert.subject.human_friendly
         self.serial = cert.serial_number
 
-    @staticmethod
-    def get_Name(name, short=False):
-        """
-            Return the distinguished name of an X509 Certificate
-            :param name: Name object to return the DN from
-            :param short: Use short form (Default: False)
-            :type name: :class:`cryptography.x509.Name`
-            :type short: Boolean
-            :rtype: str
-        """
-
-        # For the shortform, we have a lookup table
-        # See RFC4514 for more details
-        sf = {
-            "countryName": "C",
-            "stateOrProvinceName": "ST",
-            "localityName": "L",
-            "organizationalUnitName": "OU",
-            "organizationName": "O",
-            "commonName": "CN",
-            "emailAddress": "E",
-        }
-        return ", ".join(
-            ["{}={}".format(attr.oid._name if not short or attr.oid._name not in sf else sf[attr.oid._name], attr.value)
-             for
-             attr in name])
-
     def __str__(self):
         return 'Issuer: %s \n' \
                'Subject: %s \n' \
