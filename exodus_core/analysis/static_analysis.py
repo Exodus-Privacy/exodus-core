5dc3c52943afefc0e3d1a63b23234bb51bf55b1e
Pnu <6069449+simpnu@users.noreply.github.com>
2019-06-18 21:53:12 +0200

    Update to androguard 3.3.5 (#21)
    * Update to androguard 3.3.5
    
    * Use new functions from androguard for fingerprint calculation
    
    * Fix matplotlib version for python3.5
    
    * Remove unused function get_Name

--- exodus_core/analysis/static_analysis.py
+++ exodus_core/analysis/static_analysis.py
@@ -84,39 +84,10 @@ def get_details_from_gplaycli(handle):
 
 class Certificate:
     def __init__(self, cert):
-        self.fingerprint = binascii.hexlify(
-            cert.fingerprint(hashes.SHA1())
-        ).decode("ascii")
-        self.issuer = Certificate.get_Name(cert.issuer, short=False)
-        self.subject = Certificate.get_Name(cert.subject, short=False)
+        self.fingerprint = binascii.hexlify(cert.sha1).decode("ascii")
+        self.issuer = cert.issuer.human_friendly
+        self.subject = cert.subject.human_friendly
         self.serial = cert.serial_number
-
-    @staticmethod
-    def get_Name(name, short=False):
-        """
-            Return the distinguished name of an X509 Certificate
-            :param name: Name object to return the DN from
-            :param short: Use short form (Default: False)
-            :type name: :class:`cryptography.x509.Name`
-            :type short: Boolean
-            :rtype: str
-        """
-
-        # For the shortform, we have a lookup table
-        # See RFC4514 for more details
-        sf = {
-            "countryName": "C",
-            "stateOrProvinceName": "ST",
-            "localityName": "L",
-            "organizationalUnitName": "OU",
-            "organizationName": "O",
-            "commonName": "CN",
-            "emailAddress": "E",
-        }
-        return ", ".join(
-            ["{}={}".format(attr.oid._name if not short or attr.oid._name not in sf else sf[attr.oid._name], attr.value)
-             for
-             attr in name])
 
     def __str__(self):
         return 'Issuer: %s \n' \
