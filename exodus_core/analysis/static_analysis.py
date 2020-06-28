e145fc0307cdc573d91d96ebb2980fe4f9b851b5
Pnu <6069449+simpnu@users.noreply.github.com>
2020-06-28 10:24:10 +0200

    Removes internet download of app icon (#41)
    * Remove icon download from internet from core

--- exodus_core/analysis/static_analysis.py
+++ exodus_core/analysis/static_analysis.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 from collections import namedtuple
 from cryptography.x509.name import _SENTINEL, ObjectIdentifier, _NAMEOID_DEFAULT_TYPE, _ASN1Type, NameAttribute
 from hashlib import sha256, sha1
@@ -323,30 +322,6 @@ class StaticAnalysis:
 
         raise Exception('Unable to download the icon from details')
 
-    def _get_icon_from_gplay(self, handle, path):
-        """
-        Download the application icon from Google Play website
-        :param handle: application handle
-        :param path: file to be saved
-        :return: path of the saved icon
-        :raises Exception: if unable to download icon
-        """
-        address = 'https://play.google.com/store/apps/details?id=%s' % handle
-        gplay_page_content = requests.get(address).text
-        soup = BeautifulSoup(gplay_page_content, 'html.parser')
-        icon_images = soup.find_all('img', {'alt': 'Cover art'})
-        if len(icon_images) > 0:
-            icon_url = '{}'.format(icon_images[0]['src'])
-            if not icon_url.startswith('http'):
-                icon_url = 'https:{}'.format(icon_url)
-            f = requests.get(icon_url)
-            with open(path, mode='wb') as fp:
-                fp.write(f.content)
-            if os.path.isfile(path) and os.path.getsize(path) > 0:
-                return path
-        else:
-            raise Exception('Unable to download the icon from GPlay')
-
     @staticmethod
     def _render_drawable_to_png(self, bxml, path):
         ap = axml.AXMLPrinter(bxml)
@@ -370,6 +345,8 @@ class StaticAnalysis:
                 return path
         except Exception:
             logging.warning('Unable to get the icon from the APK')
+            return None
+
             # TODO: Set this back once details download is working again
             # logging.warning('Downloading icon from details')
             # try:
@@ -378,14 +355,6 @@ class StaticAnalysis:
             #     return saved_path
             # except Exception as e:
             #     logging.warning(e)
-            logging.warning('Downloading icon from Google Play')
-            try:
-                saved_path = self._get_icon_from_gplay(self.get_package(), path)
-                logging.debug('Icon downloaded from Google Play')
-                return saved_path
-            except Exception as e:
-                logging.error(e)
-        return None
 
     def get_icon_phash(self):
         """
