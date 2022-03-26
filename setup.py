01e646bcca43f7382005e314e8ccb489ce698301
pnu <simon@pruneau.info>
2022-03-26 10:26:46 +0100

    chore: Bump pillow from 9.0.0 to 9.0.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install_requires = [
     'cryptography==3.3.2',
     'dhash==1.3',
     'jellyfish==0.5.6',
-    'Pillow==9.0.0',
+    'Pillow==9.0.1',
     'requests>=2.26,<2.28',
     'six==1.15.0',
     'traitlets==4.3.2'
