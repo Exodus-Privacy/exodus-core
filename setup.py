23bce26ee67ffca3c89187dbf0d6f5858199c272
pnu <simon@pruneau.info>
2022-03-26 10:36:02 +0100

    v1.3.6

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ install_requires = [
 
 setup(
     name='exodus_core',
-    version='1.3.5',
+    version='1.3.6',
     description='Core functionality of εxodus',
     author='Exodus Privacy',
     author_email='contact@exodus-privacy.eu.org',
