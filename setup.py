0c869604667ee46b3aa00b9b6f4fe413a935de1d
Codimp <codimp@exodus-privacy.eu.org>
2025-09-27 14:31:11 +0200

    chore: Release v1.3.14

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requires = [
 
 setup(
     name='exodus_core',
-    version='1.3.13',
+    version='1.3.14',
     description='Core functionality of εxodus',
     author='Exodus Privacy',
     author_email='contact@exodus-privacy.eu.org',
