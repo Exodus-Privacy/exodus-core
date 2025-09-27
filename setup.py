29ac709e20edd507d95ff896963f6760be1a11e0
Codimp <codeurimpulsif@users.noreply.github.com>
2025-09-27 14:28:16 +0200

    [CI] Add Python 3.12 and 3.13 (#107)
    * fix pillow version
    
    * ci: add python 3.12 and 3.13
    
    * ci: fix release artifact name

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
     'cryptography==44.0.1',
     'dhash==1.4',
     'jellyfish==0.5.6',
-    'Pillow==10.3.0',
+    'Pillow==10.4.0',
     'requests>=2.26,<2.33',
     'six==1.15.0',
     'traitlets==4.3.2'
